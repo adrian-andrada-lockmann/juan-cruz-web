b65836a27a95859a8cf3af14e0ab988fb51b93b0
Adrián Andrada Lockmann <43252796+adrian-andrada-lockmann@users.noreply.github.com>
2024-06-26 16:02:03 -0300

    new repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { ReelComponent } from './reel/reel.component';
+import { TextcontentComponent } from './textcontent/textcontent.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, ReelComponent, TextcontentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
