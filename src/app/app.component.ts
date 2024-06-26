97f206acd9d9fc3a00284afed620b26d6ac3fcd4
Adrián Andrada Lockmann <43252796+adrian-andrada-lockmann@users.noreply.github.com>
2024-06-26 19:22:34 -0300

    updated code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { HeaderComponent } from './header/header.component';
 import { ReelComponent } from './reel/reel.component';
 import { TextcontentComponent } from './textcontent/textcontent.component';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -13,4 +14,31 @@ import { TextcontentComponent } from './textcontent/textcontent.component';
 })
 export class AppComponent {
   title = 'juan-cruz-web';
+  slides: any[] = [
+    {
+      url: 'assets/img/1.jpg',
+      title: 'First slide',
+      description: 'This is the first slide',
+    },
+    {
+      url: 'https://picsum.photos/200/300',
+      title: 'Second slide',
+      description: 'This is the second slide',
+    },
+    {
+      url: 'https://picsum.photos/200/300',
+      title: 'Third slide',
+      description: 'This is the third slide',
+    },
+    {
+      url: 'https://picsum.photos/200/300',
+      title: 'Fourth slide',
+      description: 'This is the fourth slide',
+    },
+    {
+      url: 'https://picsum.photos/200/300',
+      title: 'Fifth slide',
+      description: 'This is the fifth slide',
+    },
+  ];
 }
--- src/app/reel/reel.component.ts
+++ src/app/reel/reel.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CommonModule, NgClass, NgStyle } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-reel',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './reel.component.html',
   styleUrl: './reel.component.css'
 })
-export class ReelComponent {
+export class ReelComponent implements OnInit {
+  @Input() slides: any[] = [];
+  @Input() indicatorsVisible = true;
+  @Input() animationSpeed = 500;
+  @Input() autoPlay = false;
+  @Input() autoPlaySpeed = 3000;
+  currentSlide = 0;
+  faArrowRight = faArrowRight;
+  faArrowLeft = faArrowLeft;
+  hidden = false;
 
+  next() {
+    let currentSlide = (this.currentSlide + 1) % this.slides.length;
+    this.jumpToSlide(currentSlide);
+  }
+
+  previous() {
+    let currentSlide =
+      (this.currentSlide - 1 + this.slides.length) % this.slides.length;
+    this.jumpToSlide(currentSlide);
+  }
+
+  jumpToSlide(index: number) {
+    this.hidden = true;
+    setTimeout(() => {
+      this.currentSlide = index;
+      this.hidden = false;
+    }, this.animationSpeed);
+  }
+
+  ngOnInit() {
+    if (this.autoPlay) {
+      setInterval(() => {
+        this.next();
+      }, this.autoPlaySpeed);
+    }
+  }
 }
