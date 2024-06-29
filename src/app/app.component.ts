99d81cba8dcfdcf34f84fda6479693484c0f0fab
Adrián Andrada Lockmann <43252796+adrian-andrada-lockmann@users.noreply.github.com>
2024-06-28 23:19:28 -0300

    creacion de modal para Info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { ReelComponent } from './reel/reel.component';
 import { TextcontentComponent } from './textcontent/textcontent.component';
 import { SliderappComponent } from './sliderapp/sliderapp.component';
+import { SwitchService } from './services/switch.service';
 
 
 @Component({
@@ -13,23 +14,36 @@ import { SliderappComponent } from './sliderapp/sliderapp.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+
+  infoModalSwitch:boolean;
+
+
+  openModal(){
+    this.infoModalSwitch = true;
+  }
+
+  ngOnInit(): void {
+    this.modalSS.$modal.subscribe((valor)=> {this.infoModalSwitch = valor})
+  }
   title = 'juan-cruz-web';
+
   slides: any[] = [
     {
       url: 'assets/img/bancorojo.png',
-      title: 'First slide',
-      description: 'This is the first slide',
+      title: 'Banco Rojo',
+      description: 'Banco Rojo',
     },
     {
       url: 'assets/img/bancorojo2.png',
-      title: 'Second slide',
-      description: 'This is the second slide',
+      title: 'Banco Rojo 2',
+      description: 'Banco Rojo 2',
     },
     {
       url: 'assets/img/bancorojo3.png',
-      title: 'Third slide',
-      description: 'This is the third slide',
+      title: 'Banco Rojo 3',
+      description: 'Banco Rojo 3',
     },
   ];
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { SwitchService } from '../services/switch.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  constructor(private modalSS: SwitchService) { }
+
+  ngOnInit(): void {    
+
+  }
+
+
+  closeModal(){
+    this.modalSS.$modal.emit(false)
+  }
 
 }
